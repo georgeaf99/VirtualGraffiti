4e89dea36755294a09dd24b91d098ba296f49eae
georgeaf99_MacbookAir <gfarcasiu@Georges-MacBook-Air.local>
2015-02-28 22:53:21 -0500

    Stuff

--- app/src/main/java/com/gfarcasiu/virtualgrafitti/DrawActivity.java
+++ app/src/main/java/com/gfarcasiu/virtualgrafitti/DrawActivity.java
@@ -2,9 +2,14 @@ package com.gfarcasiu.virtualgrafitti;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 
 public class DrawActivity extends Activity {
 
@@ -15,6 +20,16 @@ public class DrawActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
 
         setContentView(R.layout.activity_draw);
+
+        // Set the image
+        Bundle extras = getIntent().getExtras();
+        String imageLoc = (String)extras.get("imageLocation");
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bitmap = BitmapFactory.decodeFile(imageLoc, options);
+
+        ((ImageView)(findViewById(R.id.image_view))).setImageBitmap(bitmap);
     }
 
     @Override
@@ -37,5 +52,45 @@ public class DrawActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // HELPER METHODS
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
