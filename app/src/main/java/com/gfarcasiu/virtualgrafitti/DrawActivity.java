15236acc39f214d6ff8fce6291c2355604f2f715
georgeaf99_MacbookAir <gfarcasiu@Georges-MacBook-Air.local>
2015-02-28 21:23:44 -0500

    Added new draw activity

--- app/src/main/java/com/gfarcasiu/virtualgrafitti/DrawActivity.java
+++ app/src/main/java/com/gfarcasiu/virtualgrafitti/DrawActivity.java
@@ -2,54 +2,25 @@ package com.gfarcasiu.virtualgrafitti;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
-
-public class ObserveActivity extends Activity {
-    private Camera mCamera;
-    private CameraPreview mPreview;
+public class DrawActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set landscape orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
 
-        setContentView(R.layout.activity_observe);
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        setContentView(R.layout.activity_draw);
     }
-
-    private Camera getCameraInstance() {
-        Camera c = null;
-
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-
-        return c; // returns null if camera is unavailable
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_observe, menu);
+        getMenuInflater().inflate(R.menu.menu_draw, menu);
         return true;
     }
 
